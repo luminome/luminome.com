c1fddabd17b658028b87716648193dcb6a9354e3
SAC <sac.673@gmail.com>
2023-07-26 18:40:04 +0200

    fire sale

--- src/server/main.ts
+++ src/server/main.ts
@@ -7,6 +7,10 @@ app.get("/hello", (_, res) => {
   res.send("Hello Vite + TypeScript!");
 });
 
+// app.get("/", () => {
+//   console.log('what');
+// });
+
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000...")
 );
