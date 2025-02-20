6a72e8fa348735a7a8870eadbbf89c1badc57358
SAC <sac.673@gmail.com>
2025-02-20 17:59:23 +0100

    Feb 2025 update

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,24 +1,35 @@
 // import "./style.css";
 
-import { setupCounter } from "./counter";
-import typescriptLogo from "./typescript.svg";
+const mySVG = document.querySelector<HTMLObjectElement>("#main-svg");
+let svgDoc;
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+mySVG?.addEventListener("load", function () {
+  svgDoc = mySVG.contentDocument;
+  console.log("SVG contentDocument Loaded!", mySVG);
+}, false);
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+
+// const p = document.querySelector<HTMLObjectElement>("#main-svg");
+// console.log('loaded', p?.contentDocument);
+// import { setupCounter } from "./counter";
+// import typescriptLogo from "./typescript.svg";
+
+// document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="/vite.svg" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://www.typescriptlang.org/" target="_blank">
+//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+//     </a>
+//     <h1>Vite + TypeScript</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite and TypeScript logos to learn more
+//     </p>
+//   </div>
+// `;
+
+// setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
